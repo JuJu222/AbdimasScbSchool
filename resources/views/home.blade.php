1c66931f98479f5eb38995eef3a7a6279bfb5b4a
michaellchandra <76256177+michaellchandra@users.noreply.github.com>
2021-11-04 15:08:34 +0800

    initial commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,8 +29,8 @@
         
     </div>
 
-    <div class="bg-dark text-white text-center" style="height:70px; padding-top:8px">
-        <h1>KLASIFIKASI PEMELIHARAAN DAN PERAWATAN SEKOLAH</h1>
+    <div class="bg-dark text-white text-center" style="height:70px; padding-top:15px">
+        <h2>KLASIFIKASI PEMELIHARAAN DAN PERAWATAN SEKOLAH</h2>
     </div>
 
     <div class="row container mt-3">
