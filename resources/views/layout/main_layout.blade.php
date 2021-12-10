9d107ebb6957e48e09666f60578100a29beb2705
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2021-12-10 23:24:44 +0700

    Added min-height to body css

--- resources/views/layout/main_layout.blade.php
+++ resources/views/layout/main_layout.blade.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" >
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" ></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
@@ -23,7 +23,7 @@
 
     @include('layout.navigation_layout')
 
-    <div>
+    <div class="main_content">
         @yield('main_content')
 
     </div>
