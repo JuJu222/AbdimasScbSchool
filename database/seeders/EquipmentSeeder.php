95d00bbcdf6187ac3be0339c8821e903164ae198
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2021-12-10 23:13:13 +0700

    Completed equipment and project seeder

--- database/seeders/EquipmentSeeder.php
+++ database/seeders/EquipmentSeeder.php
@@ -27,7 +27,133 @@ class EquipmentSeeder extends Seeder
         ]);
 
         DB::table('equipments')->insert([
+            'nama_equipment' => 'Pemotongan rumput',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+
+        DB::table('equipments')->insert([
+            'nama_equipment' => 'Penggulmaan',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+
+        DB::table('equipments')->insert([
+            'nama_equipment' => 'Penyiraman',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+
+        DB::table('equipments')->insert([
+            'nama_equipment' => 'Roundoup rumput liar',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+
+        DB::table('equipments')->insert([
+            'nama_equipment' => 'Pembersihan Paving Block',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+
+        DB::table('equipments')->insert([
+            'nama_equipment' => 'Drainase (Selokan)',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+
+        DB::table('equipments')->insert([
             'nama_equipment' => 'Apar',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+
+        DB::table('equipments')->insert([
+            'nama_equipment' => 'Kendaraan Transportasi',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+
+        DB::table('equipments')->insert([
+            'nama_equipment' => 'Panel Listrik',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+
+        DB::table('equipments')->insert([
+            'nama_equipment' => 'Air Conditioner 0,75 PK',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+
+        DB::table('equipments')->insert([
+            'nama_equipment' => 'Air Conditioner 1 PK',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+
+        DB::table('equipments')->insert([
+            'nama_equipment' => 'Air Conditioner 1,5 PK',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+
+        DB::table('equipments')->insert([
+            'nama_equipment' => 'Air Conditioner 2 PK',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+
+        DB::table('equipments')->insert([
+            'nama_equipment' => 'Air Conditioner 5 PK',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+
+        DB::table('equipments')->insert([
+            'nama_equipment' => 'Lampu Outdoor (Taman/Wallsign/Neonsign)',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+
+        DB::table('equipments')->insert([
+            'nama_equipment' => 'Lampu signage',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+
+        DB::table('equipments')->insert([
+            'nama_equipment' => 'Pembersihan huruf',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+
+        DB::table('equipments')->insert([
+            'nama_equipment' => 'Vacuum cleaner',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+
+        DB::table('equipments')->insert([
+            'nama_equipment' => 'Hand sanitizer',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+
+        DB::table('equipments')->insert([
+            'nama_equipment' => 'Pembersihan akrilik',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+
+        DB::table('equipments')->insert([
+            'nama_equipment' => 'Pembersihan kaca ruang',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+
+        DB::table('equipments')->insert([
+            'nama_equipment' => 'Perawatan lapangan',
             'created_at' => \Carbon\Carbon::now(),
             'updated_at' => \Carbon\Carbon::now()
         ]);
--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -19,5 +19,23 @@ class ProjectSeeder extends Seeder
             'created_at' => \Carbon\Carbon::now(),
             'updated_at' => \Carbon\Carbon::now()
         ]);
+
+        DB::table('projects')->insert([
+            'jenis_project' => 'Lanjutan pekerjaan Kantin (Rp 296.552.041 x 25%)',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+
+        DB::table('projects')->insert([
+            'jenis_project' => 'Maker space TK',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+
+        DB::table('projects')->insert([
+            'jenis_project' => 'ELZ (BOS SMP)',
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
     }
 }
