9f41bb2fdcb0797f982db484328f14b9054d9981
JuJu222 <japjustin@yahoo.com>
2022-03-15 23:31:18 +0700

    Added forgot password system

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,150 @@
-@extends('layouts.app')
+@extends('layout.main_layout', [$title = "Reset Password"])
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+@section('main_content')
+    <style>
+        section{
+            height: 100vh;
+            background: #0062E6 !important;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        .card{
+            overflow: hidden;
+            border: 0 !important;
+            border-radius: 20px !important;
+            box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.1);
+        }
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
+        .img-left{
+            width: 45%;
+            background: url('/img/front_gedung.jpeg') center;
+            background-size: cover;
+        }
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+        .card-body{
+            padding: 2rem;
+        }
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+        .title{
+            margin-bottom: 2rem;
+        }
+        .form-input{
+            position: relative;
+        }
+
+        .form-input input{
+            width: 100%;
+            height: 45px;
+            padding-left: 40px;
+            margin-bottom: 20px;
+            box-sizing: border-box;
+            box-shadow: none;
+            border: 1px solid #00000020;
+            border-radius: 50px;
+            outline: none;
+            background: transparent;
+        }
+
+        .form-input span{
+            position: absolute;
+            top: 10px;
+            padding-left: 15px;
+            color: #007bff;
+        }
+
+        .form-input input::placeholder{
+            color: black;
+            padding-left: 0px;
+        }
+
+        .form-input input:focus, .form-input input:valid{
+            border: 2px solid #007bff;
+        }
+
+        .form-input input:focus::placeholder{
+            color: #454b69;
+        }
+
+        .custom-checkbox .custom-control-input:checked  .custom-control-label::before{
+            background-color: #007bff !important;
+            border: 0px;
+        }
+
+        .form-box button[type="submit"]{
+            margin-top: 10px;
+            border: none;
+            cursor: pointer;
+            width: 100%;
+            border-radius: 50px;
+            background: #007bff;
+            color: #fff;
+            font-size: 90%;
+            letter-spacing: .1rem;
+            transition: 0.5s;
+            padding: 12px;
+        }
+
+        .form-box button[type='submit']:hover{
+            background: #0069d9;
+        }
+
+        .forget-link:hover, .register-link:hover{
+            color: #0069d9;
+            text-decoration: none;
+        }
+        .form-box {
+            text-decoration: none;
+        }
+
+        .custom-control-label{
+            text-decoration: none;
+        }
+
+
+    </style>
+    <section>
+        <div class="container">
+            <div class="row px-3">
+                <div class="col-lg-10 col-xl-9 card flex-row mx-auto px-0">
+                    <div class="img-left d-none d-md-flex"></div>
+                    <div class="card-body">
+                        <h4 class="title text-center mt-4">
+                            Reset Password
+                        </h4>
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        <form method="POST" action="{{ route('password.email') }}" class="form-box px-3">
+                            @csrf
+                            <div class="form-input">
+                                <span><i class="bi bi-envelope"></i></span>
+                                <input id="email" placeholder="Email Address" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <div class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </div>
                                 @enderror
                             </div>
-                        </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="mb-3 text-center">
+                                <button type="submit" class="btn btn-default text-uppercase ">
                                     {{ __('Send Password Reset Link') }}
                                 </button>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+
+
+    </section>
+
+
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,65 +1,169 @@
-@extends('layouts.app')
+@extends('layout.main_layout', [$title = "Reset Password"])
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+@section('main_content')
+    <style>
+        section{
+            height: 100vh;
+            background: #0062E6 !important;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
+        .card{
+            overflow: hidden;
+            border: 0 !important;
+            border-radius: 20px !important;
+            box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.1);
+        }
 
-                        <input type="hidden" name="token" value="{{ $token }}">
+        .img-left{
+            width: 45%;
+            background: url('/img/front_gedung.jpeg') center;
+            background-size: cover;
+        }
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+        .card-body{
+            padding: 2rem;
+        }
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
+        .title{
+            margin-bottom: 2rem;
+        }
+        .form-input{
+            position: relative;
+        }
+
+        .form-input input{
+            width: 100%;
+            height: 45px;
+            padding-left: 40px;
+            margin-bottom: 20px;
+            box-sizing: border-box;
+            box-shadow: none;
+            border: 1px solid #00000020;
+            border-radius: 50px;
+            outline: none;
+            background: transparent;
+        }
+
+        .form-input span{
+            position: absolute;
+            top: 10px;
+            padding-left: 15px;
+            color: #007bff;
+        }
+
+        .form-input input::placeholder{
+            color: black;
+            padding-left: 0px;
+        }
+
+        .form-input input:focus, .form-input input:valid{
+            border: 2px solid #007bff;
+        }
+
+        .form-input input:focus::placeholder{
+            color: #454b69;
+        }
+
+        .custom-checkbox .custom-control-input:checked  .custom-control-label::before{
+            background-color: #007bff !important;
+            border: 0px;
+        }
+
+        .form-box button[type="submit"]{
+            margin-top: 10px;
+            border: none;
+            cursor: pointer;
+            width: 100%;
+            border-radius: 50px;
+            background: #007bff;
+            color: #fff;
+            font-size: 90%;
+            letter-spacing: .1rem;
+            transition: 0.5s;
+            padding: 12px;
+        }
+
+        .form-box button[type='submit']:hover{
+            background: #0069d9;
+        }
+
+        .forget-link:hover, .register-link:hover{
+            color: #0069d9;
+            text-decoration: none;
+        }
+        .form-box {
+            text-decoration: none;
+        }
+
+        .custom-control-label{
+            text-decoration: none;
+        }
+
+
+    </style>
+    <section>
+        <div class="container">
+            <div class="row px-3">
+                <div class="col-lg-10 col-xl-9 card flex-row mx-auto px-0">
+                    <div class="img-left d-none d-md-flex"></div>
+                    <div class="card-body">
+                        <h4 class="title text-center mt-4">
+                            Reset Password
+                        </h4>
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        <form method="POST" action="{{ route('password.update') }}" class="form-box px-3">
+                            @csrf
+                            <input type="hidden" name="token" value="{{ $token }}">
+
+                            <div class="form-input">
+                                <span><i class="bi bi-envelope"></i></span>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="emailf" autofocus disabled>
+                                <input type="hidden" name="email" value="{{ $email ?? old('email') }}">
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <div class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </div>
                                 @enderror
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                            <div class="form-input">
+                                <span><i class="bi bi-key"></i></span>
+                                <input id="password" placeholder="Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <div class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </div>
                                 @enderror
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            <div class="form-input">
+                                <span><i class="bi bi-key"></i></span>
+                                <input id="password-confirm" placeholder="Confirm Password" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
-                        </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="mb-3 text-center">
+                                <button type="submit" class="btn btn-default text-uppercase ">
                                     {{ __('Reset Password') }}
                                 </button>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+
+
+    </section>
+
+
 @endsection
